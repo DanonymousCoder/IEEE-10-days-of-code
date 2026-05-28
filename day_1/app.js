e81aff70a367b05e16b5c540186a6880334fb0c2
Ridwanullah Baruwa <128502434+DanonymousCoder@users.noreply.github.com>
2026-05-28 12:57:55 +0100

    feat: implement task management functionality with input validation and task filtering

--- day_1/app.js
+++ day_1/app.js
@@ -1,4 +1,16 @@
 const filtBtns = document.querySelectorAll(".task-nav ul li");
+const taskName = document.getElementById("taskName");
+const submitTask = document.getElementById("submitTask");
+const inputWarning = document.querySelector(".input-warning");
+
+const completed_tasks = document.getElementById("completed");
+const active_tasks = document.getElementById("active");
+const all_tasks = document.getElementById("all");
+
+const default_body = document.querySelector(".original")
+const default_completed = document.querySelector(".no-completed")
+
+const task_display = document.querySelectorAll(".t-body");
 
 filtBtns.forEach(filtBtn => {
 
@@ -8,3 +20,64 @@ filtBtns.forEach(filtBtn => {
         filtBtn.classList.add("active")
     })
 })
+
+submitTask.addEventListener("click", () => {
+    if (taskName.value == '') {
+        inputWarning.style.display = 'block';
+    } else {
+        inputWarning.style.display = 'none';
+        console.log(taskName.value)
+    }
+})
+
+
+completed_tasks.addEventListener('click', () => {
+    task_display.forEach(element => {
+        if (!(element.classList.contains('hidden'))) {
+            element.classList.add('hidden')
+        }
+    })
+
+    default_completed.classList.remove('hidden')
+})
+
+all_tasks.addEventListener('click', () => {
+    task_display.forEach(element => {
+        if (!(element.classList.contains('hidden'))) {
+            element.classList.add('hidden')
+        }
+    })
+
+    default_body.classList.remove('hidden')
+})
+
+active_tasks.addEventListener('click', () => {
+    task_display.forEach(element => {
+        if (!(element.classList.contains('hidden'))) {
+            element.classList.add('hidden')
+        }
+    })
+
+    default_body.classList.remove('hidden')
+})
+
+
+
+const tasksCreated = [
+    {
+        "id": 0,
+        "taskName": "test",
+        "priority": "",
+        "isCompleted": false,
+        "isActive": true
+    }
+]
+
+
+function createTask() {
+    pass
+}
+
+function clearInput() {
+    taskName.value = ''
+}
